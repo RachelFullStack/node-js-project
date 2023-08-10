74aa591e1606b17f266db9c3f65309c86df21ccd
maori1994 <122039133+maori1994@users.noreply.github.com>
2023-08-10 20:46:00 +0300

    maor

---
+++ Admin/server.ts
@@ -0,0 +1,36 @@
+import express from "express";
+import mongoose from "mongoose";
+import * as dotenv from "dotenv";
+import { Schema, model } from "mongoose";
+
+dotenv.config();
+
+const app = express();
+const uri: string | undefined = process.env.MONGOOSE_URI + "playground";
+
+if (uri) {
+  mongoose
+    .connect(uri)
+    .then(() => console.log("DB connected"))
+    .catch((err) => console.log("DB error :", err));
+} else {
+  console.log("No URI");
+}
+
+app.use(express.json());
+
+const UserSchema = new Schema({ name: String, src: String });
+const UserModel = mongoose.model("users", UserSchema);
+
+app.get("/api/user-get", async (req: any, res: any) => {
+  try {
+    const users = await UserModel.find({});
+    res.send({ users });
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+app.listen(3000, () => {
+  console.log("server listen on port 3000");
+});
