ca6549b9360f9937ace991195035ec4bc9d0e4d2
ronyaakov19496 <ronyaakov19496@gmail.com>
2023-08-14 17:45:45 +0300

    ro

--- Team-project/name-welcome/server.ts
+++ Team-project/name-welcome/server.ts
@@ -1,2 +1,23 @@
-import express from "express"
+import express from "express";
+import { uuid } from "uuidv4";
+import mongoose from "mongoose";
+import * as dotenv from "dotenv";
+import {Schema, model} from "mongoose"
+
+const app = express()
+
+app.get(()=>{
+    
+})
+
+
+
+
+
+
+
+
+
+app.listen(3000)
+
 
