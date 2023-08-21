f6b568e1e1ea43aa612fb023f8e6dd706b1907b3
RachelFullStack <122094690+RachelFullStack@users.noreply.github.com>
2023-08-21 14:02:42 +0300

    update-rachel

--- Team-project/users/dist/usersRoute.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-exports.__esModule = true;
-var express_1 = require("express");
-var router = express_1["default"].Router();
-var usersController_1 = require("./usersController");
-router.post("/users/register-user", usersController_1.userRegistration);
-router.post("/users/login-user", usersController_1.userLogin);
-router.get("/users/get-user", usersController_1.getDatabaseUser);
-exports["default"] = router;
