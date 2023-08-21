3b8a7656910932ed9967656c7cffed059352114d
RachelFullStack <122094690+RachelFullStack@users.noreply.github.com>
2023-08-21 11:17:19 +0300

    rachel-update

--- team-04/client/dist/indes.js
+++ team-04/client/dist/indes.js
@@ -1,5 +1,4 @@
-//
-// // -------------------------------------------------------------------------//
+// ----------------------------------------------------------------------
 var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
     function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
     return new (P || (P = Promise))(function (resolve, reject) {
@@ -36,38 +35,6 @@ var __generator = (this && this.__generator) || function (thisArg, body) {
         if (op[0] & 5) throw op[1]; return { value: op[0] ? op[1] : void 0, done: true };
     }
 };
-function handleShowUser(eve) {
-    return __awaiter(this, void 0, void 0, function () {
-        var response, data, databaseUser, userHtml, error_1;
-        return __generator(this, function (_a) {
-            switch (_a.label) {
-                case 0:
-                    _a.trys.push([0, 3, , 4]);
-                    return [4 /*yield*/, fetch("/api/users/get-user")];
-                case 1:
-                    response = _a.sent();
-                    return [4 /*yield*/, response.json()];
-                case 2:
-                    data = _a.sent();
-                    console.log("data", data);
-                    databaseUser = data.databaseUser;
-                    userHtml = document.querySelector("#userName");
-                    if (!databaseUser)
-                        throw new Error("problem with Showing Database User function");
-                    if (!userHtml)
-                        throw new Error("No user element on DOM");
-                    userHtml.innerHTML = databaseUser.userName;
-                    return [3 /*break*/, 4];
-                case 3:
-                    error_1 = _a.sent();
-                    console.log(error_1);
-                    return [3 /*break*/, 4];
-                case 4: return [2 /*return*/];
-            }
-        });
-    });
-}
-// ----------------------------------------------------------------------
 // Signup/register
 function register(eve) {
     eve.preventDefault();
@@ -136,6 +103,38 @@ function login(eve) {
     catch (error) {
         console.error(error);
     }
+}
+// // -------------------------------------------------------------------------//
+function handleShowUser(eve) {
+    return __awaiter(this, void 0, void 0, function () {
+        var response, data, databaseUser, userHtml, error_1;
+        return __generator(this, function (_a) {
+            switch (_a.label) {
+                case 0:
+                    _a.trys.push([0, 3, , 4]);
+                    return [4 /*yield*/, fetch("/api/users/get-user")];
+                case 1:
+                    response = _a.sent();
+                    return [4 /*yield*/, response.json()];
+                case 2:
+                    data = _a.sent();
+                    console.log("data", data);
+                    databaseUser = data.databaseUser;
+                    userHtml = document.querySelector("#userName");
+                    if (!databaseUser)
+                        throw new Error("problem with Showing Database User function");
+                    if (!userHtml)
+                        throw new Error("No user element on DOM");
+                    userHtml.innerHTML = databaseUser.userName;
+                    return [3 /*break*/, 4];
+                case 3:
+                    error_1 = _a.sent();
+                    console.log(error_1);
+                    return [3 /*break*/, 4];
+                case 4: return [2 /*return*/];
+            }
+        });
+    });
 }
 // ------------------------------------------------------------------------------------
 // Signup
--- team-04/client/indes.ts
+++ team-04/client/indes.ts
@@ -1,23 +1,3 @@
-//
-// // -------------------------------------------------------------------------//
-
-async function handleShowUser(eve: any) {
-  try {
-    const response = await fetch("/api/users/get-user");
-    const data = await response.json();
-    console.log("data", data);
-    const { databaseUser } = data;
-    const userHtml = document.querySelector("#userName") as HTMLDivElement;
-
-    if (!databaseUser)
-      throw new Error("problem with Showing Database User function");
-    if (!userHtml) throw new Error("No user element on DOM");
-    userHtml.innerHTML = databaseUser.userName;
-  } catch (error) {
-    console.log(error);
-  }
-}
-
 // ----------------------------------------------------------------------
 
 // Signup/register
@@ -89,6 +69,24 @@ function login(eve: any) {
       });
   } catch (error) {
     console.error(error);
+  }
+}
+// // -------------------------------------------------------------------------//
+
+async function handleShowUser(eve: any) {
+  try {
+    const response = await fetch("/api/users/get-user");
+    const data = await response.json();
+    console.log("data", data);
+    const { databaseUser } = data;
+    const userHtml = document.querySelector("#userName") as HTMLDivElement;
+
+    if (!databaseUser)
+      throw new Error("problem with Showing Database User function");
+    if (!userHtml) throw new Error("No user element on DOM");
+    userHtml.innerHTML = databaseUser.userName;
+  } catch (error) {
+    console.log(error);
   }
 }
 
